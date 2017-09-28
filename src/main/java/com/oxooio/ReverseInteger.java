90691fc544ce91fadf6094fba120eded370ff7a1
Sam <sam.iagd@hotmail.com>
2017-09-28 16:14:20 +0800

    优化数字反转算法

--- src/main/java/com/oxooio/ReverseInteger.java
+++ src/main/java/com/oxooio/ReverseInteger.java
@@ -3,12 +3,10 @@ package com.oxooio;
 /**
  * Reverse digits of an integer.
  *
- * Example1: x = 123, return 321
- * Example2: x = -123, return -321
+ * Example1: x = 123, return 321 Example2: x = -123, return -321
  *
- * Note:
- * The input is assumed to be a 32-bit signed integer.
- * Your function should return 0 when the reversed integer overflows.
+ * Note: The input is assumed to be a 32-bit signed integer. Your function should return 0 when the
+ * reversed integer overflows.
  *
  * @author Sam
  * @since 3.1.0
@@ -16,16 +14,24 @@ package com.oxooio;
 public class ReverseInteger {
 
   public static void main(String[] args) {
-    System.out.println(new ReverseInteger().reverse(-2147483648));
-    System.out.println(new ReverseInteger().reverse(Integer.MAX_VALUE - 1));
-    System.out.println(new ReverseInteger().reverse(-1253));
+    ReverseInteger ri = new ReverseInteger();
+//    System.out.println(ri.reverse(-2147483648));
+//    System.out.println(ri.reverse(Integer.MAX_VALUE - 1));
+//    System.out.println(ri.reverse(-1253));
+
+//    System.out.println(ri.reverse2(123));
+//    System.out.println(ri.reverse2(-123));
+//    System.out.println(ri.reverse2(-2147483648));
+    System.out.println(ri.reverse2(2147483647));
   }
 
   /**
-   *  here is my ugly code, very sad.
+   * 最简单的一种思路：将入参x转换成由十进制的每一个字符组成的
+   * 数组，然后对数组做首尾交换。位移需要处理的就是符号位与溢出
+   * 的风险。
    *
-   * @param x
-   * @return
+   * @param x 十进制输入
+   * @return 反转之后的十进制
    */
   public int reverse(int x) {
     if (x == Integer.MIN_VALUE) {
@@ -47,5 +53,19 @@ public class ReverseInteger {
       return reversedLong < Integer.MIN_VALUE ? 0 : reversedLong.intValue();
     }
     return reversedLong > Integer.MAX_VALUE ? 0 : reversedLong.intValue();
+  }
+
+  /**
+   * 从十进制的低位往高位开始分离计算反转数。
+   * @param x
+   * @return
+   */
+  public int reverse2(int x) {
+    Long sum = 0L;
+    while (x != 0) {
+      sum = sum * 10 + x % 10;
+      x /= 10;
+    }
+    return (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) ? 0 : sum.intValue();
   }
 }
